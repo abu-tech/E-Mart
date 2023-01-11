aeab4fbd3a62df79ab27cc17410d9d3b840bbc6d
Mohammed samir <abutech8801@gmail.com>
2023-01-11 12:16:35 +0530

    ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,18 @@ app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get("/", (req, res) => {
+        res.json({message: "API is Running!"})
+    })
+}
+
 app.use(errorHandler)
 
 app.listen(PORT, () => {
